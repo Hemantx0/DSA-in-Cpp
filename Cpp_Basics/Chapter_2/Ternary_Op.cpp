5d86cf3d8fc3d5bc33bd0160cf663b16d05daea6
Hemantx0 <sahuhemant7878@gmail.com>
2026-03-20 23:55:21 +0530

    learn about ternary operator

---
+++ Cpp_Basics/Chapter_2/Ternary_Op.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter no : ";
+    int x;
+    cin>>x;
+   //  (condition) ? if true : if false ;
+    (x%2==0) ? cout<<"EVEN" : cout<<"ODD" ;
+}
